9285569f65f2153b805ba8c7d36308f4b0eb1175
haramasif <harammalikk49@gmail.com>
2024-11-10 18:47:30 +0500

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div className='bg-[#A29875] w-[1560px] h-[128px] text-[white] text-[75px]'>
+         MANZZARI
+      </div>
+    </div>
+  );
+};
+
+export default Header;
